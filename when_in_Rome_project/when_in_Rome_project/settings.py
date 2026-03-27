b7418133b568965b01b994e13753a19bf5dd1663
Eimantas Bogusis <3015017b@student.gla.ac.uk>
2026-03-27 15:00:03 +0000

    Deployment

--- when_in_Rome_project/when_in_Rome_project/settings.py
+++ when_in_Rome_project/when_in_Rome_project/settings.py
@@ -74,7 +74,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
